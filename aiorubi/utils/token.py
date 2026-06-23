8a4aa54182122c9d9cdf051fb5e7b60908e3f405
amirsf <amirsf0101@gmail.com>
2026-06-23 15:41:06 +0330

    Add utils/token.py

---
+++ aiorubi/utils/token.py
@@ -0,0 +1,24 @@
+from functools import lru_cache
+
+
+class TokenValidationError(Exception):
+    pass
+
+
+@lru_cache
+def validate_token(token: str) -> bool:
+    """
+    Validate Rubika token
+
+    :param token:
+    :return:
+    """
+    if not isinstance(token, str):
+        msg = f"Token is invalid! It must be 'str' type instead of {type(token)} type."
+        raise TokenValidationError(msg)
+
+    if any(x.isspace() for x in token):
+        message = "Token is invalid! It can't contains spaces."
+        raise TokenValidationError(message)
+
+    return True
